96f14c54a73ee85382182ebc7200e474fa742f5c
James Thorne <james@jthorne.co.uk>
2015-01-24 16:57:50 +0000

    split request builder into partial classes

---
+++ JumpKick.HttpLib/JumpKick.HttpLib/Builder/RequestBuilder.Action.cs
@@ -0,0 +1,101 @@
+﻿using JumpKick.HttpLib.Provider;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpKick.HttpLib.Builder
+{
+    public partial class RequestBuilder
+    {
+
+        #region Actions
+        ActionProvider actionProvider;
+        Action<WebHeaderCollection, Stream> success;
+        Action<WebException> fail;
+        public RequestBuilder OnSuccess(Action<WebHeaderCollection, String> action)
+        {
+            this.success = (headers, stream) =>
+            {
+                StreamReader reader = new StreamReader(stream);
+                action(headers, reader.ReadToEnd());
+            };
+
+
+            return this;
+        }
+
+        public RequestBuilder OnSuccess(Action<String> action)
+        {
+            this.success = (headers, stream) =>
+            {
+                StreamReader reader = new StreamReader(stream);
+                action(reader.ReadToEnd());
+            };
+            return this;
+        }
+
+
+        public RequestBuilder OnSuccess(Action<WebHeaderCollection, Stream> action)
+        {
+            this.success = action;
+            return this;
+        }
+
+        public RequestBuilder DownloadTo(String filePath)
+        {
+#if NETFX_CORE
+            test
+#else
+
+
+            this.success = (headers, result) =>
+            {
+
+
+                FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate);
+                result.CopyTo(fs);
+                fs.Close();
+            };
+            return this;
+#endif
+        }
+
+
+        public RequestBuilder AppendTo(String filePath)
+        {
+            this.success = (headers, result) =>
+            {
+                FileStream fs = new FileStream(filePath, FileMode.Append);
+                result.CopyTo(fs);
+                fs.Close();
+            };
+            return this;
+        }
+
+
+        public RequestBuilder OnFail(Action<WebException> action)
+        {
+            fail = action;
+            return this;
+        }
+
+
+        public RequestBuilder Action(ActionProvider action)
+        {
+            actionProvider = action;
+            return this;
+        }
+
+        public ActionProvider GetAction()
+        {
+            return actionProvider;
+        }
+
+        #endregion
+  
+    }
+}
---
+++ JumpKick.HttpLib/JumpKick.HttpLib/Builder/RequestBuilder.Auth.cs
@@ -0,0 +1,35 @@
+﻿using JumpKick.HttpLib.Provider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpKick.HttpLib.Builder
+{
+    public partial class RequestBuilder
+    {
+
+        #region Auth
+        private AuthenticationProvider authProvider;
+        public RequestBuilder Auth(string username, string password)
+        {
+            authProvider = new BasicAuthenticationProvider(username, password);
+            return this;
+        }
+
+        public RequestBuilder Auth(string text)
+        {
+            authProvider = new TextAuthenticationProvider(text);
+            return this;
+        }
+
+        public RequestBuilder Auth(AuthenticationProvider provider)
+        {
+            authProvider = provider;
+            return this;
+        }
+        #endregion
+
+    }
+}
