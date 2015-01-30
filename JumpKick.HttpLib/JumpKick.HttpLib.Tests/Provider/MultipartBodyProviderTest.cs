a01f7855354504e58e6dbe42d62d472e5fdd0c11
James Thorne <james@jthorne.co.uk>
2015-01-30 22:13:29 +0000

    Fixed test

--- JumpKick.HttpLib/JumpKick.HttpLib.Tests/Provider/MultipartBodyProviderTest.cs
+++ JumpKick.HttpLib/JumpKick.HttpLib.Tests/Provider/MultipartBodyProviderTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using JumpKick.HttpLib.Provider;
 using System.IO;
 using System.Text.RegularExpressions;
+using Moq;
 
 namespace JumpKick.HttpLib.Tests.Provider
 {
@@ -11,16 +12,26 @@ namespace JumpKick.HttpLib.Tests.Provider
     {
         Stream dummyFileStream;
         MultipartBodyProvider provider;
+        Stream dummyFileStream2;
         StreamWriter w;
         [TestInitialize]
         public void SetUp()
         {
             provider = new MultipartBodyProvider();
             dummyFileStream = new MemoryStream();
+            dummyFileStream2 = new MemoryStream();
             w = new StreamWriter(dummyFileStream);
             w.Write("file");
             w.Flush();
+
+            w = new StreamWriter(dummyFileStream2);
+            w.Write("file");
+            w.Flush();
+
+
             dummyFileStream.Seek(0, SeekOrigin.Begin);
+
+            dummyFileStream2.Seek(0, SeekOrigin.Begin);
         }
 
         [TestMethod]
@@ -94,7 +105,7 @@ namespace JumpKick.HttpLib.Tests.Provider
         public void TestPostBodyContainsOneSeparatorPerFile()
         {
             provider.AddFile(new NamedFileStream("a", "b", "c", dummyFileStream));
-            provider.AddFile(new NamedFileStream("a", "b", "c", dummyFileStream));
+            provider.AddFile(new NamedFileStream("a", "b", "c", dummyFileStream2));
 
             StreamReader r = new StreamReader(provider.GetBody());
             String content = r.ReadToEnd();
