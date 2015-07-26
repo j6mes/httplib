46388f1476fee0a7975770d5e1ed1b96b3d6e52d
James Thorne <james@jthorne.co.uk>
2015-07-26 23:05:27 +0100

    Removed assembly info

--- JumpKick.HttpLib/JumpKick.HttpLib.Portable/Properties/AssemblyInfo.cs
+++ JumpKick.HttpLib/JumpKick.HttpLib.Portable/Properties/AssemblyInfo.cs
@@ -20,9 +20,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1c7164a9-4deb-4511-be22-5790b4d594a9")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
