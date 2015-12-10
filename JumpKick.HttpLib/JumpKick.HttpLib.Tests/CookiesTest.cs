dadfec0f1847516db4fd2cbd66e4f4b86b59a207
James Thorne <james@jthorne.co.uk>
2015-12-10 15:27:59 +0000

    Added test for cookies

---
+++ JumpKick.HttpLib/JumpKick.HttpLib.Tests/CookiesTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpKick.HttpLib.Tests
+{
+    [TestClass]
+    public class CookiesTest
+    {
+        [TestMethod]
+        public void TestInitHasCookieContainer()
+        {
+            Assert.IsNotNull(Cookies.Container);
+        }
+
+        [TestMethod]
+        public void TestClearCookiesReturnsNewContainer()
+        {
+            CookieContainer c = Cookies.Container;
+
+            Cookies.ClearCookies();
+            Assert.AreNotEqual(c,Cookies.Container);
+        }
+
+        [TestMethod]
+        public void TestSetCookieContainer()
+        {
+            CookieContainer c =  new CookieContainer();
+
+            Cookies.SetCookies(c);
+            Assert.AreEqual(c,Cookies.Container);
+        }
+    }
+}
